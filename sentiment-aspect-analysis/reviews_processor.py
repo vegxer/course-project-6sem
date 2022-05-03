0adbc6e0aacd101b00cac01bf5a20842afb39dd0
mak <maxim.krupin@itdt.ru>
2022-05-03 09:54:04 +0300

    д

--- sentiment-aspect-analysis/reviews_processor.py
+++ sentiment-aspect-analysis/reviews_processor.py
@@ -4,23 +4,134 @@ import re
 import numpy
 import spacy
 from joblib import Parallel, delayed
+from keras.preprocessing.sequence import pad_sequences
 from keras.utils.np_utils import to_categorical
+from nltk import SnowballStemmer
+
+nlp = spacy.load("ru_core_news_sm")
+stemmer = SnowballStemmer("russian")
 
 
-def get_list_of_reviews_and_y_res(reviews):
+def num_to_text(num):
+    if num == 1:
+        return 'oneStar'
+    if num == 2:
+        return 'twoStar'
+    if num == 3:
+        return 'threeStar'
+    if num == 4:
+        return 'fourStar'
+    return 'fiveStar'
+
+
+def get_statistics(path, save_path):
+    with open(path, "r", encoding="utf-8") as dataset:
+        reviews = json.load(dataset)
+    statistics = {}
+    statistics['count'] = 0
+    statistics['plot'] = {}
+    statistics['plot']['oneStar'] = 0
+    statistics['plot']['twoStar'] = 0
+    statistics['plot']['threeStar'] = 0
+    statistics['plot']['fourStar'] = 0
+    statistics['plot']['fiveStar'] = 0
+
+    statistics['music'] = {}
+    statistics['music']['oneStar'] = 0
+    statistics['music']['twoStar'] = 0
+    statistics['music']['threeStar'] = 0
+    statistics['music']['fourStar'] = 0
+    statistics['music']['fiveStar'] = 0
+
+    statistics['originality'] = {}
+    statistics['originality']['oneStar'] = 0
+    statistics['originality']['twoStar'] = 0
+    statistics['originality']['threeStar'] = 0
+    statistics['originality']['fourStar'] = 0
+    statistics['originality']['fiveStar'] = 0
+
+    statistics['spectacularity'] = {}
+    statistics['spectacularity']['oneStar'] = 0
+    statistics['spectacularity']['twoStar'] = 0
+    statistics['spectacularity']['threeStar'] = 0
+    statistics['spectacularity']['fourStar'] = 0
+    statistics['spectacularity']['fiveStar'] = 0
+
+    statistics['actors'] = {}
+    statistics['actors']['oneStar'] = 0
+    statistics['actors']['twoStar'] = 0
+    statistics['actors']['threeStar'] = 0
+    statistics['actors']['fourStar'] = 0
+    statistics['actors']['fiveStar'] = 0
+
+    statistics['rating'] = {}
+    statistics['rating']['oneStar'] = 0
+    statistics['rating']['twoStar'] = 0
+    statistics['rating']['threeStar'] = 0
+    statistics['rating']['fourStar'] = 0
+    statistics['rating']['fiveStar'] = 0
+
+    for review in reviews:
+        if review['plot'] is not None and review['music'] is not None and review['actors'] is not None and review['originality'] is not None and review['spectacularity'] is not None:
+            statistics['count'] += 1
+            try:
+                statistics['rating'][num_to_text(review['rating'])] += 1
+            except Exception:
+                print("err")
+            statistics['plot'][num_to_text(review['plot'])] += 1
+            statistics['music'][num_to_text(review['music'])] += 1
+            statistics['actors'][num_to_text(review['actors'])] += 1
+            statistics['originality'][num_to_text(review['originality'])] += 1
+            statistics['spectacularity'][num_to_text(review['spectacularity'])] += 1
+
+    with open(save_path, "w", encoding="utf-8") as file:
+        json.dump(statistics, file)
+
+
+def scale_to_five_grade(grade):
+    return to_categorical(grade - 1, num_classes=5)
+
+
+def scale_to_three_grade(grade):
+    grades = [0, 0, 0]
+    if grade < 3:
+        grades[0] = 1
+    elif grade == 3:
+        grades[1] = 1
+    else:
+        grades[2] = 1
+    return grades
+
+
+def scale_to_two_grade(grade):
+    grades = [0, 0]
+    if grade < 3:
+        grades[0] = 1
+    else:
+        grades[1] = 1
+    return grades
+
+
+def get_list_of_reviews_and_y_res(reviews, aspect, grade_scale):
+    scale_func = scale_to_five_grade if grade_scale == 5 else scale_to_three_grade if grade_scale == 3 else scale_to_two_grade
     y_res = []
     processed_reviews = []
+    corrupted_reviews = 0
     for review in reviews:
-        review_text = "достоинства: " + review['plus'].lower() + "\n" + \
-                      "недостатки: " + review['minus'].lower() + "\n" + \
-                      "отзыв: " + review['body'].lower()
-        processed_reviews.append(review_text)
-        y_res.append(to_categorical(review['rating'] - 1, num_classes=5))
+        try:
+            review_text = 'рекомендация: ' + review['recommend'] + "\n\n" + \
+                          "достоинства: " + review['plus'].lower() + "\n\n" + \
+                          "недостатки: " + review['minus'].lower() + "\n\n" + \
+                          "отзыв: " + review['body'].lower()
+            y_res.append(scale_func(review[aspect]))
+            processed_reviews.append(review_text)
+        except Exception:
+            corrupted_reviews += 1
+    print("Повреждённых отзывов: " + str(corrupted_reviews))
     return processed_reviews, y_res
 
 
 def pos_tag_review(review):
-    nlp = spacy.load("ru_core_news_sm")
     tagged = []
     tokens = nlp(review)
     for token in tokens:
@@ -32,11 +143,23 @@ def pos_tag_reviews(reviews, threads_num):
     return Parallel(n_jobs=threads_num)(delayed(pos_tag_review)(review) for review in reviews)
 
 
+def pos_tag_review_unpossed(review):
+    tagged = []
+    tokens = nlp(review)
+    for token in tokens:
+        tagged.append(stemmer.stem(token.lemma_))
+    return tagged
+
+
+def pos_tag_reviews_unpossed(reviews, threads_num):
+    return Parallel(n_jobs=threads_num)(delayed(pos_tag_review_unpossed)(review) for review in reviews)
+
+
 def clean_review(review):
     cleaned_review = []
     for word in review:
-        if len(word[0]) > 1 and word[1] in ['PROPN', 'ADJ', 'VERB', 'NOUN']:
-            cleaned_review.append(word[0])
+        if (len(word[0]) == 1 and word[0].isdigit()) or (len(word[0]) > 1 and word[1] in ['PROPN', 'ADJ', 'VERB', 'NOUN']):
+            cleaned_review.append(stemmer.stem(word[0]))
     return cleaned_review
 
 
@@ -59,7 +182,7 @@ def create_sorted_dictionary(reviews):
                 dictionary[word] = 1
             else:
                 dictionary[word] += 1
-    dictionary = dict([item for item in dictionary.items() if item[1] > 3])
+    dictionary = dict([item for item in dictionary.items() if item[1] > 2])
     sorted_dict = list(sorted(dictionary.items(), key=lambda item: item[1]))
     dict_len = len(sorted_dict)
     tuples = []
@@ -79,7 +202,7 @@ def parallel_encode(review, dictionary):
 def encode_frequent(reviews, threads_num):
     dictionary = create_sorted_dictionary(reviews)
     encoded_reviews = Parallel(n_jobs=threads_num)(delayed(parallel_encode)(review, dictionary) for review in reviews)
-    return len(dictionary), encoded_reviews
+    return dictionary, encoded_reviews
 
 
 def write_to_file(X_res, y_res, save_path):
@@ -92,25 +215,61 @@ def write_to_file(X_res, y_res, save_path):
         save_file.write("\n")
         for y in y_res:
             for star in y:
-                save_file.write(str(star))
+                save_file.write(str(float(star)))
                 save_file.write(" ")
             save_file.write("\n")
 
 
-def generate_dataset(path_to_text_dataset, path_to_save_numeric_dataset, threads_num, review_length=300):
+def save_dictionary(save_path, dictionary):
+    with open(save_path, "w", encoding="utf-8") as save_file:
+        for item in dictionary.items():
+            save_file.write(item[0] + " " + str(item[1]))
+            save_file.write("\n")
+
+
+def generate_dataset(path_to_text_dataset, path_to_save_numeric_dataset, aspect, grade_scale, threads_num, review_length=250):
     with open(path_to_text_dataset, "r", encoding="utf-8") as dataset:
-        processed_reviews, y_res = get_list_of_reviews_and_y_res(json.load(dataset))
+        processed_reviews, y_res = get_list_of_reviews_and_y_res(json.load(dataset), aspect, grade_scale)
     processed_reviews = pos_tag_reviews(processed_reviews, threads_num)
     processed_reviews = clean_reviews(processed_reviews, threads_num)
     processed_reviews = cut_reviews(processed_reviews, review_length)
-    vocabulary_size, X_res = encode_frequent(processed_reviews, threads_num)
-    print("Vocab size: " + str(vocabulary_size))
+    dictionary, X_res = encode_frequent(processed_reviews, threads_num)
+    print("Vocab size: " + str(len(dictionary)))
 
     zipped = list(zip(X_res, y_res))
     random.shuffle(zipped)
     X_res, y_res = zip(*zipped)
 
     write_to_file(X_res, y_res, path_to_save_numeric_dataset)
+    # save_dictionary("dictionary.txt", dictionary)
+
+
+def digitize_reviews(reviews, dictionary):
+    digitized = []
+    for i in range(len(reviews)):
+        digitized.append([])
+        for word in reviews[i]:
+            if word in dictionary:
+                digitized[i].append(dictionary[word])
+    return digitized
+
+
+def load_text_dataset(path_to_text_dataset, dictionary, review_length, aspect, grade_scale, threads_num):
+    with open(path_to_text_dataset, "r", encoding="utf-8") as dataset:
+        processed_reviews, y_res = get_list_of_reviews_and_y_res(json.load(dataset), aspect, grade_scale)
+    processed_reviews = pos_tag_reviews_unpossed(processed_reviews, threads_num)
+    processed_reviews = digitize_reviews(processed_reviews, dictionary)
+    processed_reviews = pad_sequences(numpy.asarray(processed_reviews), maxlen=review_length)
+    return processed_reviews, y_res
+
+
+def read_dictionary(path):
+    dictionary = {}
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            item = line.split(' ')
+            dictionary[item[0]] = int(item[1])
+    return dictionary
 
 
 def load_dataset(path_to_numeric_dataset):
--- sentiment-aspect-analysis/train.py
+++ sentiment-aspect-analysis/train.py
@@ -1,9 +1,12 @@
+import keras.layers
+from keras.optimizer_v2.adam import Adam
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, SimpleRNN, GRU, Bidirectional, GlobalMaxPool1D, Dropout, Conv1D, MaxPooling1D, \
+    Flatten, GlobalMaxPool2D, BatchNormalization
 from keras.layers.embeddings import Embedding
-from keras import metrics
+from keras import metrics, Input
 from keras import models
 from datetime import datetime
 from keras.callbacks import ModelCheckpoint
@@ -13,38 +16,54 @@ from reviews_processor import load_dataset, write_to_file, get_model_accuracy
 if __name__ == "__main__":
     print("Time of start: " + datetime.now().strftime("%H:%M:%S"))
 
-    X_dataset, y_dataset = load_dataset("processed_dataset.txt")
-    X_train, X_test, y_train, y_test = train_test_split(X_dataset, y_dataset, random_state=11, test_size=0.25)
+    # X_train, y_train = load_dataset("reviewsTrainDigit2.txt")
+    # X_test, y_test = load_dataset("reviewsTestDigit2.txt")
+    X_dataset, y_dataset = load_dataset("grade_three/digit_datasets/plot.txt")
+    X_train, X_test, y_train, y_test = train_test_split(X_dataset, y_dataset, random_state=42, test_size=0.2)
 
-    words_per_review = 300
+    words_per_review = 250
 
     X_train = pad_sequences(X_train, maxlen=words_per_review)
     X_test = pad_sequences(X_test, maxlen=words_per_review)
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, random_state=11, test_size=0.30)
+    # X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, random_state=42, test_size=0.30)
     rnn = Sequential()
-    rnn.add(Embedding(input_dim=28704, output_dim=128, input_length=words_per_review))
-    rnn.add(LSTM(units=128, dropout=0.2, recurrent_dropout=0.2))
-    rnn.add(Dense(units=5, activation='sigmoid'))
+    rnn.add(Input(shape=(250,)))
+    rnn.add(Embedding(input_dim=39731, output_dim=250, input_length=words_per_review))
+    rnn.add(Bidirectional(LSTM(units=250, return_sequences=True)))
+    rnn.add(GlobalMaxPool1D())
+    rnn.add(Dense(units=250, activation='tanh'))
+    rnn.add(Dropout(0.5))
+    rnn.add(Dense(units=3, activation='sigmoid'))
+
     rnn.compile(optimizer='adam',
                 loss='categorical_crossentropy',
                 metrics=[metrics.Accuracy(), metrics.Precision(), metrics.Recall(), metrics.CategoricalAccuracy(),
                          metrics.CategoricalCrossentropy(), metrics.MeanSquaredError()])
     rnn.summary()
-    chkpt = ModelCheckpoint('best_model',
+    chkpt = ModelCheckpoint('grade_three/models/plot_model_best',
                             monitor='val_loss',
                             verbose=1,
                             save_best_only=True,
                             mode='auto')
-    rnn.fit(X_train, y_train, epochs=25, batch_size=32, validation_data=(X_val, y_val), callbacks=[chkpt])
-    rnn.save('model')
+    rnn.fit(X_train, y_train, epochs=2, batch_size=32, validation_data=(X_test, y_test), callbacks=[chkpt])
+    rnn.save('grade_three/models/plot_model')
     try:
-        write_to_file(X_test, y_test, "training_test_set.txt")
-    except:
+        write_to_file(X_test, y_test, "test_set.txt")
+    except Exception:
         print("Ошибка записи тестового датасета")
     results = rnn.evaluate(X_test, y_test)
-    print("Results: ")
-    print(results)
+    print("Результаты: ")
+    print("loss: " + str(results[0]))
+    print("accuracy: " + str(results[1]))
+    precision = float(results[2])
+    recall = float(results[3])
+    print("precision: " + str(precision))
+    print("recall: " + str(recall))
+    print("F1: " + str((2 * precision * recall) / (precision + recall)))
+    print("categorical accuracy: " + str(results[4]))
+    print("categorical crossentropy: " + str(results[5]))
+    print("mean squared error: " + str(results[6]))
 
-    print("Точность: " + str(get_model_accuracy(models.load_model("model"), X_test, y_test)))
+    # print("Точность: " + str(get_model_accuracy(models.load_model("plot_model2"), X_test, y_test)))
 
     print("Time of end: " + datetime.now().strftime("%H:%M:%S"))
